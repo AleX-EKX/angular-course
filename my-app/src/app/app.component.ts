652a2d8f3f3cc11bdc931b0789015eac32388d7f
Alex <100437540+AleX-EKX@users.noreply.github.com>
2024-07-14 16:00:14 +0400

    task-1.7

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  constructor(private titleService: Title, private metaService: Meta) { }
+  ngOnInit(): void {
+    this.titleService.setTitle('Desk Page');
+    this.metaService.addTag({ name: 'og:desc', content: 'root_desc' });
+  }
 }

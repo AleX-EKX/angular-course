a739dbe0ddb62cf8436c271d5578dad0d6bca0ef
Alex <100437540+AleX-EKX@users.noreply.github.com>
2024-07-21 17:23:47 +0400

    task-2.2

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
 
+
+interface Books {
+  name: string | null;
+  author: string | null;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+  
+  
 export class AppComponent {
-  title = 'my-app';
+  books: Books[] = [
+    {
+      name: 'Дубровский',
+      author: 'Пушкин',
+    },
+    {
+      name: 'Мцыри',
+      author: 'Лермонтов',
+    },
+  ];
+
+  create_book = {
+    name: null,
+    author: null,
+  };
+
+  createBook() {
+    this.books.push({
+      name: this.create_book.name,
+      author: this.create_book.author,
+    });
+  }
   
 }
